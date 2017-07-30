4040fcc57f4a3237a2e1e7251a67f49025a0e8e4
Alex Chan <alexchantastic@gmail.com>
2017-07-29 23:20:58 -0700

    Updated gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,25 @@ var gulp = require('gulp');
 var sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     csso = require('gulp-csso'),
-    concat = require('gulp-concat'),
+    webpack = require('webpack-stream'),
+    named = require('vinyl-named'),
     uglify = require('gulp-uglify'),
     browsersync = require('browser-sync'),
+    del = require('del'),
     run = require('run-sequence');
 
 var paths = {
   'css': {
     'src': 'styles/src/',
-    'dist': 'styles/'
+    'dist': 'styles/dist/'
   },
   'js': {
     'src': 'js/src/',
-    'dist': 'js/'
+    'dist': 'js/dist/'
   }
 };
 
-gulp.task('css', function() {
+gulp.task('css', ['clean:css'], function() {
   return gulp.src(paths.css.src + '**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
@@ -29,21 +31,22 @@ gulp.task('css', function() {
     .pipe(browsersync.reload({stream: true}));
 });
 
-gulp.task('js', function() {
+gulp.task('js', ['clean:js'], function() {
   return gulp.src(paths.js.src + '**/*.js')
-    .pipe(concat('core.js'))
+    .pipe(named())
+    .pipe(webpack())
     .pipe(gulp.dest(paths.js.dist))
     .pipe(browsersync.reload({stream: true}));
 });
 
 gulp.task('minify:css', function() {
-  return gulp.src(paths.css.dist + '*.css')
+  return gulp.src(paths.css.dist + '**/*.css')
     .pipe(csso())
     .pipe(gulp.dest(paths.css.dist));
 });
 
 gulp.task('minify:js', function() {
-  return gulp.src(paths.js.dist + '*.js')
+  return gulp.src(paths.js.dist + '**/*.js')
     .pipe(uglify())
     .pipe(gulp.dest(paths.js.dist));
 });
@@ -52,7 +55,8 @@ gulp.task('minify', ['minify:css', 'minify:js']);
 
 gulp.task('server', function() {
   browsersync.init({
-    server: {baseDir: '.'}
+    server: {baseDir: '.'},
+    open: false
   });
 });
 
@@ -66,6 +70,16 @@ gulp.task('watch', ['server'], function() {
   gulp.watch(paths.js.src + '**/*.js', ['js']);
 
   gulp.watch('./*.html', ['server:reload']);
+});
+
+gulp.task('clean', ['clean:css', 'clean:js']);
+
+gulp.task('clean:css', function() {
+  return del(paths.css.dist  + '**/*');
+});
+
+gulp.task('clean:js', function() {
+  return del(paths.js.dist + '**/*');
 });
 
 gulp.task('build', ['css', 'js']);
