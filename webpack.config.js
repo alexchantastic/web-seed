8678d140def27846cbd454c31dd08b71cf0e677e
Alex Chan <alexchantastic@gmail.com>
2018-04-20 17:45:24 -0700

    webpack-dev-server configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,34 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin'),
+      HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin'),
+      webpack = require('webpack');
+
 module.exports = {
   mode: 'development',
   entry: {
     app: './src/js/app.js'
   },
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
 
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      alwaysWriteToDisk: true,
+      template: 'index.html'
+    }),
+    new HtmlWebpackHarddiskPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true,
+    watchContentBase: true
   }
 };
