f9bcabdfa2c546d6af09a35e85a8c88b38e97701
Alex Chan <alexchantastic@gmail.com>
2018-04-20 19:28:51 -0700

    Clean dist before build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin'),
+const CleanWebpackPlugin = require('clean-webpack-plugin'),
+      HtmlWebpackPlugin = require('html-webpack-plugin'),
       webpack = require('webpack');
 
 let devServer;
@@ -55,6 +56,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(['./dist']),
     new HtmlWebpackPlugin({
       template: './src/public/index.html'
     })
