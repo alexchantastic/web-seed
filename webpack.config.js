2aed3d0990571b76a67607caef9f4e3e977ce0c0
Alex Chan <alex.chan@fireeye.com>
2018-07-13 15:33:40 -0700

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
+        }
+      },
+      {
         test: /\.(svg|png|jpg|gif)$/,
         include: /images/,
         use: [
